4780dbbfaa237288c0d2543e8be2505525c9e425
Jorge Corral Ortiz-Coronado <Jorge Corral Ortiz-Coronado>
2017-04-18 18:57:36 +0200

    All removed, starting all over again...

--- spq03/Servidor/group3_facebook/src/es/deusto/ingenieria/sd/facebook/remote/IFacebookFacade.java
+++
@@ -1,9 +0,0 @@
-package es.deusto.ingenieria.sd.facebook.remote;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IFacebookFacade extends Remote{
-
-	public boolean loginAutenticar(String nick, String contrasenia)throws RemoteException;
-}
